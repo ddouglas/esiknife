c176698a26b635c279858a18edf1316a462ca1d5
David Douglas <ddouglas@douglaswebdev.net>
2018-05-04 03:23:34 -0400

    Add configuration for MemberAccess Model

--- app/Models/MemberAccess.php
+++ app/Models/MemberAccess.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class MemberAccess extends Model
 {
-    //
+    protected $primaryKey = 'member_id';
+    protected $table = 'member_accesses';
+    public $incrementing = false;
+    protected static $unguarded = true;
+
+    protected $dates = [
+        'expires'
+    ];
 }
