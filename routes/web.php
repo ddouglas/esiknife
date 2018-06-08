a24a9d866e3a0efabca0676be980747060e53e7b
David Douglas <ddouglas@douglaswebdev.net>
2018-06-08 05:15:08 -0400

    Moved Settings from the PortalController to the SettingsController since they will be account specific instead of character specific. I think this is how this should be :-D

--- routes/web.php
+++ routes/web.php
@@ -18,27 +18,25 @@ Route::group(['middleware' => ['guest']], function () {
 Route::group(['middleware' => ['auth']], function () {
 
     Route::match(['GET'], '/dashboard', 'PortalController@dashboard')->name('dashboard');
-
-    Route::match(['GET'], '/assets', 'PortalController@assets')->name('assets');
-    Route::match(['GET'], '/bookmarks', 'PortalController@bookmarks')->name('bookmarks');
-    Route::match(['GET'], '/clones', 'PortalController@clones')->name('clones');
-    Route::match(['GET'], '/contacts', 'PortalController@contacts')->name('contacts');
-    Route::match(['GET'], '/contracts', 'PortalController@contracts')->name('contracts');
-    Route::match(['GET'], '/contract/{id}', 'PortalController@contract')->name('contract.view');
-    Route::match(['GET'], '/contracts/interactions', 'PortalController@contractInteractions')->name('contracts.interactions');
-    Route::match(['GET'], '/mails', 'PortalController@mails')->name('mails');
-    Route::match(['GET'], '/mail/{id}', 'PortalController@mail')->name('mail');
-    Route::match(['GET'], '/skills', 'PortalController@skills')->name('skillz');
-    Route::match(['GET'], '/skills/flyable', 'PortalController@flyable')->name('skillz.flyable');
-    Route::match(['GET'], '/skillqueue', 'PortalController@queue')->name('skillqueue');
-    Route::match(['GET'], '/wallet/transactions', 'PortalController@wallet_transactions')->name('wallet.transactions');
-    Route::match(['GET'], '/wallet/journal', 'PortalController@wallet_journal')->name('wallet.journal');
-
-    Route::match(['GET', 'POST'], '/access', 'PortalController@access')->name('access');
-
     Route::match(['GET'], '/settings', 'SettingController@index')->name('settings.index');
     Route::match(['GET', 'DELETE'], '/settings/token', 'SettingController@token')->name('settings.token');
+    Route::match(['GET', 'POST'], '/settings/access', 'SettingController@access')->name('access');
 
+    Route::match(['GET'], '/{id}/overview', 'PortalController@overview')->name('overview');
+    Route::match(['GET'], '/{id}/assets', 'PortalController@assets')->name('assets');
+    Route::match(['GET'], '/{id}/bookmarks', 'PortalController@bookmarks')->name('bookmarks');
+    Route::match(['GET'], '/{id}/clones', 'PortalController@clones')->name('clones');
+    Route::match(['GET'], '/{id}/contacts', 'PortalController@contacts')->name('contacts');
+    Route::match(['GET'], '/{id}/contracts', 'PortalController@contracts')->name('contracts');
+    Route::match(['GET'], '/{id}/contract/{contract_id}', 'PortalController@contract')->name('contract.view');
+    Route::match(['GET'], '/{id}/contracts/interactions', 'PortalController@contractInteractions')->name('contracts.interactions');
+    Route::match(['GET'], '/{id}/mails', 'PortalController@mails')->name('mails');
+    Route::match(['GET'], '/{id}/mail/{mail_id}', 'PortalController@mail')->name('mail');
+    Route::match(['GET'], '/{id}/skills', 'PortalController@skills')->name('skillz');
+    Route::match(['GET'], '/{id}/skills/flyable', 'PortalController@flyable')->name('skillz.flyable');
+    Route::match(['GET'], '/{id}/skillqueue', 'PortalController@queue')->name('skillqueue');
+    Route::match(['GET'], '/{id}/wallet/transactions', 'PortalController@wallet_transactions')->name('wallet.transactions');
+    Route::match(['GET'], '/{id}/wallet/journal', 'PortalController@wallet_journal')->name('wallet.journal');
 });
 
 
