d9ddac0724850f0711a6006002c893b188c217f8
David Douglas <ddouglas@douglaswebdev.net>
2018-06-08 15:37:21 -0400

    Updated URLs

--- routes/web.php
+++ routes/web.php
@@ -20,23 +20,23 @@ Route::group(['middleware' => ['auth']], function () {
     Route::match(['GET'], '/dashboard', 'PortalController@dashboard')->name('dashboard');
     Route::match(['GET'], '/settings', 'SettingController@index')->name('settings.index');
     Route::match(['GET', 'DELETE'], '/settings/token', 'SettingController@token')->name('settings.token');
-    Route::match(['GET', 'POST'], '/settings/access', 'SettingController@access')->name('access');
+    Route::match(['GET', 'POST'], '/settings/access', 'SettingController@access')->name('settings.access');
 
-    Route::match(['GET'], '/{id}/overview', 'PortalController@overview')->name('overview');
-    Route::match(['GET'], '/{id}/assets', 'PortalController@assets')->name('assets');
-    Route::match(['GET'], '/{id}/bookmarks', 'PortalController@bookmarks')->name('bookmarks');
-    Route::match(['GET'], '/{id}/clones', 'PortalController@clones')->name('clones');
-    Route::match(['GET'], '/{id}/contacts', 'PortalController@contacts')->name('contacts');
-    Route::match(['GET'], '/{id}/contracts', 'PortalController@contracts')->name('contracts');
-    Route::match(['GET'], '/{id}/contract/{contract_id}', 'PortalController@contract')->name('contract.view');
-    Route::match(['GET'], '/{id}/contracts/interactions', 'PortalController@contractInteractions')->name('contracts.interactions');
-    Route::match(['GET'], '/{id}/mails', 'PortalController@mails')->name('mails');
-    Route::match(['GET'], '/{id}/mail/{mail_id}', 'PortalController@mail')->name('mail');
-    Route::match(['GET'], '/{id}/skills', 'PortalController@skills')->name('skillz');
-    Route::match(['GET'], '/{id}/skills/flyable', 'PortalController@flyable')->name('skillz.flyable');
-    Route::match(['GET'], '/{id}/skillqueue', 'PortalController@queue')->name('skillqueue');
-    Route::match(['GET'], '/{id}/wallet/transactions', 'PortalController@wallet_transactions')->name('wallet.transactions');
-    Route::match(['GET'], '/{id}/wallet/journal', 'PortalController@wallet_journal')->name('wallet.journal');
+    Route::match(['GET'], '/{member}/overview', 'PortalController@overview')->name('overview');
+    Route::match(['GET'], '/{member}/assets', 'PortalController@assets')->name('assets')->middleware('authorized:Somethign');
+    Route::match(['GET'], '/{member}/bookmarks', 'PortalController@bookmarks')->name('bookmarks');
+    Route::match(['GET'], '/{member}/clones', 'PortalController@clones')->name('clones');
+    Route::match(['GET'], '/{member}/contacts', 'PortalController@contacts')->name('contacts');
+    Route::match(['GET'], '/{member}/contracts', 'PortalController@contracts')->name('contracts');
+    Route::match(['GET'], '/{member}/contract/{contract_id}', 'PortalController@contract')->name('contract.view');
+    Route::match(['GET'], '/{member}/contracts/interactions', 'PortalController@contractInteractions')->name('contracts.interactions');
+    Route::match(['GET'], '/{member}/mails', 'PortalController@mails')->name('mails');
+    Route::match(['GET'], '/{member}/mail/{mail_id}', 'PortalController@mail')->name('mail');
+    Route::match(['GET'], '/{member}/skills', 'PortalController@skills')->name('skillz');
+    Route::match(['GET'], '/{member}/skills/flyable', 'PortalController@flyable')->name('skillz.flyable');
+    Route::match(['GET'], '/{member}/skillqueue', 'PortalController@queue')->name('skillqueue');
+    Route::match(['GET'], '/{member}/wallet/transactions', 'PortalController@wallet_transactions')->name('wallet.transactions');
+    Route::match(['GET'], '/{member}/wallet/journal', 'PortalController@wallet_journal')->name('wallet.journal');
 });
 
 
