a8c6bd2200f41e9a24d3a3ca12c3e75ef2a663ac
David Douglas <ddouglas@douglaswebdev.net>
2018-06-08 17:54:32 -0400

    Code Clean Up

--- resources/views/portal/welcome.blade.php
+++ resources/views/portal/welcome.blade.php
@@ -21,83 +21,86 @@
                     <div class="row">
                         <div class="col-md-6">
                             <ul class="list-group">
-                                <li class="list-group-item text-center"><strong>Private Information</strong></li>
+                                <li class="list-group-item">
+                                    <div class="float-right">
+                                        <a href="#" id="all">[Select All]</a>
+                                    </div>
+                                    <strong>Character Information</strong>
+                                </li>
                                 <label for="readCharacterAssets" class="mb-0">
                                     <li class="list-group-item py-auto">
-                                        <input type="checkbox" id="readCharacterAssets" name="scopes[readCharacterAssets]" /> <span class="ml-2">Read Character Assets</span>
+                                        <input type="checkbox" id="readCharacterAssets" name="scopes[readCharacterAssets]" class="item" /> <span class="ml-2">Read Character Assets</span>
                                     </li>
                                 </label>
 
                                 <label for="readCharacterBookmarks" class="mb-0">
                                     <li class="list-group-item py-auto">
-                                        <input type="checkbox" id="readCharacterBookmarks" name="scopes[readCharacterBookmarks]" /> <span class="ml-2">Read Character Bookmarks</span>
+                                        <input type="checkbox" id="readCharacterBookmarks" name="scopes[readCharacterBookmarks]" class="item" /> <span class="ml-2">Read Character Bookmarks</span>
                                     </li>
                                 </label>
 
                                 <label for="readCharacterClones" class="mb-0">
                                     <li class="list-group-item py-auto">
-                                        <input type="checkbox" id="readCharacterClones" name="scopes[readCharacterClones]" /> <span class="ml-2">Read Character Clones</span>
+                                        <input type="checkbox" id="readCharacterClones" name="scopes[readCharacterClones]" class="item" /> <span class="ml-2">Read Character Clones</span>
+                                    </li>
+                                </label>
+
+                                <label for="readCharacterContacts" class="mb-0">
+                                    <li class="list-group-item py-auto">
+                                        <input type="checkbox" id="readCharacterContacts" name="scopes[readCharacterContacts]" class="item" /> <span class="ml-2">Read Character Contacts & Standings</span>
                                     </li>
                                 </label>
 
                                 <label for="readCharacterContracts" class="mb-0">
                                     <li class="list-group-item py-auto">
-                                        <input type="checkbox" id="readCharacterContracts" name="scopes[readCharacterContracts]" /> <span class="ml-2">Read Character Contracts</span>
+                                        <input type="checkbox" id="readCharacterContracts" name="scopes[readCharacterContracts]" class="item" /> <span class="ml-2">Read Character Contracts</span>
                                     </li>
                                 </label>
 
                                 <label for="readCharacterImplants" class="mb-0">
                                     <li class="list-group-item py-auto">
-                                        <input type="checkbox" id="readCharacterImplants" name="scopes[readCharacterImplants]" /> <span class="ml-2">Read Character Implants</span>
+                                        <input type="checkbox" id="readCharacterImplants" name="scopes[readCharacterImplants]" class="item" /> <span class="ml-2">Read Character Implants</span>
                                     </li>
                                 </label>
 
                                 <label for="readCharacterLocation" class="mb-0">
                                     <li class="list-group-item py-auto">
-                                        <input type="checkbox" id="readCharacterLocation" name="scopes[readCharacterLocation]" /> <span class="ml-2">Read Character Location</span>
+                                        <input type="checkbox" id="readCharacterLocation" name="scopes[readCharacterLocation]" class="item" /> <span class="ml-2">Read Character Location</span>
+                                    </li>
+                                </label>
+
+                                <label for="readCharacterMails" class="mb-0">
+                                    <li class="list-group-item py-auto">
+                                        <input type="checkbox" id="readCharacterMails" name="scopes[readCharacterMails]" class="item" /> <span class="ml-2">Read Character Mails</span>
                                     </li>
                                 </label>
 
                                 <label for="readCharacterSkills" class="mb-0">
                                     <li class="list-group-item py-auto">
-                                        <input type="checkbox" id="readCharacterSkills" name="scopes[readCharacterSkills]" /> <span class="ml-2">Read Character Skills</span>
+                                        <input type="checkbox" id="readCharacterSkills" name="scopes[readCharacterSkills]" class="item" /> <span class="ml-2">Read Character Skills</span>
                                     </li>
                                 </label>
 
                                 <label for="readCharacterSkillQueue" class="mb-0">
                                     <li class="list-group-item py-auto">
-                                        <input type="checkbox" id="readCharacterSkillQueue" name="scopes[readCharacterSkillQueue]" /> <span class="ml-2">Read Character Skill Queue</span>
+                                        <input type="checkbox" id="readCharacterSkillQueue" name="scopes[readCharacterSkillQueue]" class="item" /> <span class="ml-2">Read Character Skill Queue</span>
                                     </li>
                                 </label>
 
                                 <label for="readCharacterShip" class="mb-0">
                                     <li class="list-group-item py-auto ">
-                                        <input type="checkbox" id="readCharacterShip" name="scopes[readCharacterShip]" /> <span class="ml-2">Read Character Ship</span>
+                                        <input type="checkbox" id="readCharacterShip" name="scopes[readCharacterShip]" class="item" /> <span class="ml-2">Read Character Ship</span>
                                     </li>
                                 </label>
 
                                 <label for="readCharacterWallet" class="mb-0">
                                     <li class="list-group-item py-auto">
-                                        <input type="checkbox" id="readCharacterWallet" name="scopes[readCharacterWallet]" /> <span class="ml-2">Read Character Wallet</span>
+                                        <input type="checkbox" id="readCharacterWallet" name="scopes[readCharacterWallet]" class="item" /> <span class="ml-2">Read Character Wallet</span>
                                     </li>
                                 </label>
                             </ul>
                         </div>
                         <div class="col-lg-6">
-                            <ul class="list-group">
-                                <li class="list-group-item text-center"><strong>Communications</strong></li>
-                                <label for="readCharacterContacts" class="mb-0">
-                                    <li class="list-group-item py-auto">
-                                        <input type="checkbox" id="readCharacterContacts" name="scopes[readCharacterContacts]" /> <span class="ml-2">Read Character Contacts & Standings</span>
-                                    </li>
-                                </label>
-                                <label for="readCharacterMails" class="mb-0">
-                                    <li class="list-group-item py-auto">
-                                        <input type="checkbox" id="readCharacterMails" name="scopes[readCharacterMails]" /> <span class="ml-2">Read Character Mails</span>
-                                    </li>
-                                </label>
-                            </ul>
-                            <hr />
                             <ul class="list-group">
                                 <li class="list-group-item text-center"><strong>Utility Classes</strong></li>
                                 <label for="readUniverseStructures" class="mb-0">
@@ -121,23 +124,12 @@
             </div>
         </div>
     </div>
+@endsection
 
-    {{-- <div class="row">
-        <div class="col-md-6">
-            <label for="readCharacterNotifications">
-                <li class="list-group-item py-auto">
-                    <input type="checkbox" id="readCharacterNotifications" name="scopes[readCharacterNotifications]" disabled /> <span class="ml-2">Read Character Notifications</span>
-                </li>
-            </label>
-            <label for="readCharacterMails">
-                <li class="list-group-item py-auto">
-                    <input type="checkbox" id="readCharacterMails" name="scopes[readCharacterMails]" disabled /> <span class="ml-2">Read Character Mails</span>
-                </li>
-            </label>
-        </div>
-        <div class="col-lg-6">
-
-
-        </div>
-    </div> --}}
+@section('js')
+    <script>
+        $('#all').on('click', function(){
+            $(':checkbox.item').prop('checked', true);
+        });
+    </script>
 @endsection
