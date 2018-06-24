13b4ebd3e67bba4d0965b3baa5605701b15c551a
David Douglas <ddouglas@douglaswebdev.net>
2018-06-24 14:37:14 -0400

    Gave bookmarks some love. Now there is some statistical data displayed out.

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -109,8 +109,17 @@ class PortalController extends Controller
 
     public function bookmarks ($member) {
         $member = Member::findOrFail($member);
-        $member->load('bookmarks', 'bookmarkFolders');
-        return view('portal.bookmarks')->withMember($member);
+        $member->load('bookmarkFolders.bookmarks');
+
+        $uniqueLocations = $member->bookmarks->pluck('location')->unique('id')->keyBy('id');
+
+        foreach ($uniqueLocations as $location) {
+            $count = $member->bookmarks->where('location_id', $location->id)->count();
+            $uniqueLocations->get($location->id)->count = $count;
+        }
+        $uniqueLocations = $uniqueLocations->sortByDesc('count');
+
+        return view('portal.bookmarks')->withMember($member)->withUniqueLocations($uniqueLocations);
     }
 
     public function clones ($member)
