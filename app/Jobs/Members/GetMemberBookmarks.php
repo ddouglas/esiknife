566f2ddc3f3124ae61d1baeddc83fdbb26c0c171
David Douglas <ddouglas@douglaswebdev.net>
2018-07-16 05:33:39 -0400

    ESI Bookmarks caps out at 1K bookmarks. These changes replicate the Assets functionality and allow us to pull beyond the intiail 1K that we get back

--- app/Jobs/Members/GetMemberBookmarks.php
+++ app/Jobs/Members/GetMemberBookmarks.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
+use Bus;
 use ESIK\Models\Member;
 use ESIK\Traits\Trackable;
 use ESIK\Http\Controllers\DataController;
@@ -16,17 +17,24 @@ class GetMemberBookmarks implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, Trackable;
 
+    public $id, $page, $dataCont;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct(int $id)
+    public function __construct(int $id, int $page = null)
     {
         $this->id = $id;
+        $this->page = $page;
         $this->dataCont = new DataController;
         $this->prepareStatus();
+        if (!is_null($page)) {
+            $this->setInput(['id' => $id, 'page' => $page]);
+        } else {
+            $this->setInput(['id' => $id]);
+        }
     }
 
     /**
@@ -37,11 +45,41 @@ class GetMemberBookmarks implements ShouldQueue
     public function handle()
     {
         $member = Member::findOrFail($this->id);
-        $getMemberBookmarks = $this->dataCont->getMemberBookmarks($member);
-        $status = $getMemberBookmarks->status;
-        $payload = $getMemberBookmarks->payload;
-        if (!$status) {
-            throw new \Exception($payload->message, 1);
+        if (is_null($this->page)) {
+            $headMemberAssets = $this->dataCont->headMemberBookmarks($member);
+            $status = $headMemberAssets->status;
+            $payload = $headMemberAssets->payload;
+            if (!$status) {
+                throw new \Exception($payload->message, 1);
+            }
+
+            $responseHeaders = collect($payload->headers->response)->recursive();
+            if ($responseHeaders->has('X-Pages')) {
+                $pages = (int)$responseHeaders->get('X-Pages');
+            } else {
+                throw new \Exception("X-Pages header is missing from response", 1);
+            }
+
+            $folders = $this->dataCont->getMemberBookmarkFolders($member);
+
+            if (isset($pages)) {
+                $dispatchedJobs = collect(); $now = now();
+                for($x=1;$x<=$pages;$x++) {
+                    $job = new \ESIK\Jobs\Members\GetMemberBookmarks($member->id, $x);
+                    $job->delay($now);
+                    Bus::dispatch($job);
+                    $dispatchedJobs->push($job->getJobStatusId());
+                    $now = $now->addSeconds(1);
+                }
+                $member->jobs()->attach($dispatchedJobs->toArray());
+            }
+        } else {
+            $getMemberAssets = $this->dataCont->getMemberBookmarksByPage($member, $this->page);
+            $status = $getMemberAssets->status;
+            $payload = $getMemberAssets->payload;
+            if (!$status) {
+                throw new \Exception($payload->message, 1);
+            }
         }
     }
 }
