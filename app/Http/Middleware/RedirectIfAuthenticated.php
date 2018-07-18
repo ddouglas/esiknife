b028818b88db5485afab764b25081e5b78160bea
David Douglas <ddouglas@douglaswebdev.net>
2018-07-17 20:30:47 -0400

    Small bug patch for pre dashboard days

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,9 +18,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(route('overview'));
+            return redirect(route('dashboard'));
         }
-
         return $next($request);
     }
 }
