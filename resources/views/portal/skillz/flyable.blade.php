a9c51f2487a299d2bbba1399dd374a361fa3dcb5
David Davaham <ddouglas@douglaswebdev.net>
2018-09-08 05:52:25 -0400

    Forgot to commit this.

--- resources/views/portal/skillz/flyable.blade.php
+++ resources/views/portal/skillz/flyable.blade.php
@@ -10,66 +10,63 @@
         <div class="row">
             <div class="col-lg-12">
                 @foreach ($flyable as $value)
-                    <h3>
-                        {{ $value->get('name') }} ({{ $value->get('ships')->get('can')->count() }} / {{ $value->get('ships')->get('can')->count() + $value->get('ships')->get('cant')->count() }})
-                        <hr />
-                    </h3>
-                    <br />
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <h5 class="text-center">Can Fly<hr /></h5>
-                            <div class="row justify-content-center mb-3">
-                                @forelse ($value->get('ships')->get('can') as $can)
-                                    <div class="col-lg-2">
-                                        <div class="card">
-                                            <img src="{{ config('services.eve.urls.img') }}/Render/{{ $can->id }}_128.png" class="card-img-top" title="{{ $can->name }}" />
-                                            <div class="card-body">
-                                                <p class="card-text">
-                                                    {{ $can->name }}
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                @empty
-                                    <div class="col-lg-12">
-                                        <p class="text-white">
-                                            Unfortunately, this character cannot fly any of the ships in the {{ $value->get('name') }} ship
-                                        </p>
-
-                                    </div>
-                                @endforelse
-                                <hr />
-                            </div>
-
+                    <h3 class="text-center">
+                        <div class="float-right">
+                            <a href="#" data-toggle="collapse" data-target="#{{ $value->get('key') }}_body">
+                                <i class="fas fa-bars"></i>
+                            </a>
                         </div>
-
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <h5 class="text-center">Cant Fly<hr /></h5>
-                            <div class="row">
-                                @forelse ($value->get('ships')->get('cant') as $cant)
-                                    <div class="col-lg-2">
-                                        <div class="card">
-                                            <img src="{{ config('services.eve.urls.img') }}/Render/{{ $cant->id }}_128.png" class="card-img-top" title="{{ $cant->name }}" />
-                                            <div class="card-body">
-                                                <p class="card-text">
-                                                    {{ $cant->name }}
-                                                </p>
+                        {{ $value->get('name') }} ({{ $value->get('ships')->get('can')->count() }} / {{ $value->get('ships')->get('can')->count() + $value->get('ships')->get('cant')->count() }})
+                        <hr class="white" />
+                    </h3>
+                    <div class="collapse" id="{{ $value->get('key') }}_body">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h5 class="text-center">Can Fly ({{ $value->get('ships')->get('can')->count() }}) <hr class="white" /></h5>
+                                <div class="list-group mb-3">
+                                    @forelse ($value->get('ships')->get('can') as $can)
+                                        <div class="col-lg-12">
+                                            <div class="list-group-item">
+                                                <div class="media mt-0">
+                                                    <img src="{{ config('services.eve.urls.img') }}/Render/{{ $can->id }}_32.png" class="rounded img-fluid mr-3" />
+                                                    <div class="media-body align-center">
+                                                        <h4>{{ $can->name }}</h4>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                @empty
-                                    <div class="col-lg-12">
-                                        <p class="text-white">
-                                            This is epic. This character can fly every ship in the {{ $value->get('name') }} ship group
-                                        </p>
-
-                                    </div>
-                                @endforelse
+                                    @empty
+                                        <div class="col-lg-12">
+                                            <p class="text-white text-center">
+                                                Unfortunately, {{ $member->info->name }} cannot fly any of the ships in the {{ $value->get('name') }} ship group
+                                            </p>
+                                        </div>
+                                    @endforelse
+                                </div>
                             </div>
-                            <hr />
-
+                            <div class="col-lg-6">
+                                <h5 class="text-center">Can't Fly ({{ $value->get('ships')->get('cant')->count() }})<hr class="white" /></h5>
+                                <div class="list-group mb-3">
+                                    @forelse ($value->get('ships')->get('cant') as $cant)
+                                        <div class="col-lg-12">
+                                            <div class="list-group-item">
+                                                <div class="media mt-0">
+                                                    <img src="{{ config('services.eve.urls.img') }}/Render/{{ $cant->id }}_32.png" class="rounded img-fluid mr-3" />
+                                                    <div class="media-body align-center">
+                                                        <h4>{{ $cant->name }}</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @empty
+                                        <div class="col-lg-12">
+                                            <p class="text-white text-center">
+                                                {{ $member->info->name }} can fly all of the ship in the {{ $value->get('name') }} ship group.
+                                            </p>
+                                        </div>
+                                    @endforelse
+                                </div>
+                            </div>
                         </div>
                     </div>
                 @endforeach
